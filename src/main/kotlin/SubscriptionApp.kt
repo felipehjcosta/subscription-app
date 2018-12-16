742693f1a36c8fe75806bed5e3dbfb360488ccdd
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-12-16 13:14:22 -0200

    Create a verify post handler

--- src/main/kotlin/SubscriptionApp.kt
+++ src/main/kotlin/SubscriptionApp.kt
@@ -1,8 +1,10 @@
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.http.ContentType
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -15,6 +17,9 @@ fun Application.module() {
     routing {
         get("/") {
             call.respondText("Hello World", ContentType.Text.Html)
+        }
+        post("/verify") {
+            call.respond("OK")
         }
     }
 }
