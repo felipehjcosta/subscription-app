95adbf7cb0efa7d7113da36e4c07ce132f3c1d8f
felipehjcosta <felipe.h.j.costa@gmail.com>
2018-12-19 10:08:42 -0200

    Move verify path to inside of subscription path

--- src/main/kotlin/web/SubscriptionResource.kt
+++ src/main/kotlin/web/SubscriptionResource.kt
@@ -6,14 +6,17 @@ import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.post
+import io.ktor.routing.route
 import model.Request
 import service.SubscriptionsService
 
 fun Routing.subscription(service: SubscriptionsService) {
-    post("/verify") {
-        val request = call.receive<Request>()
-        service.findSubscriptions(request.userId, request.productId)?.let { subscription ->
-            call.respond(subscription)
-        } ?: call.respond(HttpStatusCode.NotFound, "Subscription Invalid")
+    route("/subscription") {
+        post("/verify") {
+            val request = call.receive<Request>()
+            service.findSubscriptions(request.userId, request.productId)?.let { subscription ->
+                call.respond(subscription)
+            } ?: call.respond(HttpStatusCode.NotFound, "Subscription Invalid")
+        }
     }
 }
